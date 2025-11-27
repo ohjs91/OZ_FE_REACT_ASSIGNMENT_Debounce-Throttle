cab2f0ba5d3fc1f166573e656a846f83c77cafe6
진석 <ozjinseok@gmail.com>
2025-11-27 12:55:51 +0900

    debounce와 throttle

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [query, setQuery] = useState("");
+  const [debounceQuery, setDebounceQuery] = useState("");
+  const [throttleQuery, setThrottleQuery] = useState("");
   const [searchString, setSearchString] = useState("");
+  const time = useRef(new Date());
 
-  const handleChange = (event) => {
-    setQuery(event.target.value);
-    console.log("검색 쿼리:", event.target.value);
+  const debounceHandleChange = (event) => {
+    setDebounceQuery(event.target.value);
+  };
+  const throttleHandleChange = (event) => {
+    setThrottleQuery(event.target.value);
   };
 
+  useEffect(() => {
+    const debouneTimer = setTimeout(() => {
+      console.log("debounce 검색 쿼리:", debounceQuery);
+    }, 1000);
+
+    return () => clearTimeout(debouneTimer);
+  }, [debounceQuery]);
+
+  useEffect(() => {
+    const newTime = new Date();
+    const delay = 1000 - (newTime - time.current);
+    const throttleTimer = setTimeout(() => {
+      console.log("throttle 검색 쿼리:", throttleQuery);
+      time.current = new Date();
+    }, delay);
+
+    return () => clearTimeout(throttleTimer);
+  }, [throttleQuery]);
   return (
     <div className="container">
       <h1>
@@ -22,7 +44,7 @@ function App() {
         <input
           type="text"
           placeholder="Debounce를 이용한 검색..."
-          onChange={handleChange}
+          onChange={debounceHandleChange}
         />
       </div>
       <div>
@@ -30,7 +52,7 @@ function App() {
         <input
           type="text"
           placeholder="Throttle을 이용한 검색..."
-          onChange={handleChange}
+          onChange={throttleHandleChange}
         />
       </div>
       <p>{searchString}</p>
